6e5dd71dc2f97bca2693aac5cb9622e733cb3c6b
andrew-303 <a_biyanliang@126.com>
2020-01-29 11:31:31 +0800

    设计模式-中介者模式

--- src/main/java/com/bjsxt/designpattern/mediator/President.java
+++ src/main/java/com/bjsxt/designpattern/mediator/President.java
@@ -3,6 +3,9 @@ package com.bjsxt.designpattern.mediator;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * 中介者具体实现类
+ */
 public class President implements Mediator{
 
     private Map<String,Department> map = new HashMap<String, Department>();
